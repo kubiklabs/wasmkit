2cec3d5f34cc7c7aba3a952671798a38deb295e1
udit-gulati <uditgulati6@gmail.com>
2023-04-14 22:39:42 +0530

    use wasmkit-ast-types to have wasmKitTypes in ts artifacts

--- packages/wasmkit/src/lib/compile/tsSchema.ts
+++ packages/wasmkit/src/lib/compile/tsSchema.ts
@@ -4,8 +4,8 @@ import { pascal } from "case";
 import { writeFileSync } from 'fs';
 import { sync as mkdirp } from "mkdirp";
 import { join } from "path";
-import * as w from 'polar-wasm-ast-types';
-import { getMessageProperties } from "polar-wasm-ast-types";
+import * as w from 'wasmkit-ast-types';
+import { getMessageProperties } from "wasmkit-ast-types";
 
 import { findAndParseTypes, findExecuteMsg, findQueryMsg } from "./utils";
 
