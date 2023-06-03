9e51ba84067f986d772d633b821dfcdb116c2ce3
udit-gulati <uditgulati6@gmail.com>
2023-06-04 01:34:20 +0530

    update default gas values

--- packages/wasmkit/src/lib/constants.ts
+++ packages/wasmkit/src/lib/constants.ts
@@ -4,19 +4,19 @@ export const WASMKIT_NAME = "wasmkit";
 
 export const defaultFees = {
   upload: {
-    amount: [{ amount: "250000", denom: "untrn" }],
+    amount: [{ amount: "850000", denom: "untrn" }],
     gas: "1000000"
   },
   init: {
-    amount: [{ amount: "125000", denom: "untrn" }],
+    amount: [{ amount: "225000", denom: "untrn" }],
     gas: "500000"
   },
   exec: {
-    amount: [{ amount: "50000", denom: "untrn" }],
+    amount: [{ amount: "70000", denom: "untrn" }],
     gas: "200000"
   },
   send: {
-    amount: [{ amount: "20000", denom: "untrn" }],
+    amount: [{ amount: "50000", denom: "untrn" }],
     gas: "80000"
   }
 };
